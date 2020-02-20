b9e5a22616a8025fe6e2a6e962f5a72dd91e9a13
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-20 13:11:52 -0500

    fix test

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -27,19 +27,19 @@ describe('PhoneCat Application', function() {
 
       cy.get('@q').type('tablet')
       cy.ng('binding', 'phone.name')
-        .should(function($names){
-          expect(getNames($names)).to.deep.eq [
+        .should(function ($names) {
+          expect(getNames($names), 'default order of names').to.deep.eq([
             'Motorola XOOM™ with Wi-Fi',
             'MOTOROLA XOOM™'
-          ]
+          ])
         })
       cy.ng('model', '$ctrl.orderProp').select('name')
       cy.ng('binding', 'phone.name')
-        .should(function($names){
-          expect(getNames($names)).to.deep.eq [
+        .should(function ($names) {
+          expect(getNames($names)).to.deep.eq([
             'MOTOROLA XOOM™',
             'Motorola XOOM™ with Wi-Fi'
-          ]
+          ])
         })
     })
 
